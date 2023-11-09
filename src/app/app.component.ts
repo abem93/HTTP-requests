14e9ea97cfa4db8cdb7c19094f355683bf2ecfea
abem93 <aimejia4@gmail.com>
2023-11-09 04:01:22 -0600

    Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'http';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.http
+      .post<{ name: string }>(
+        'https://ng-complete-guide-c8cb1-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    this.isFetching = true;
+    // Send Http request
+    this.http
+      .get<{ [key: string]: Post }>(
+        'https://ng-complete-guide-c8cb1-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts
+      });
+
+
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
 }
